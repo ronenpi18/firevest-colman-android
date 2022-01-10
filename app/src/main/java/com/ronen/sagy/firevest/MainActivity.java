0a9d017348341ce923c8c138bd2c9c664138a136
Ronen Pinhasov <ronen.pinhasov@cyberark.com>
2022-01-10 20:46:47 +0200

    nav bar added with fragments

--- app/src/main/java/com/ronen/sagy/firevest/MainActivity.java
+++ app/src/main/java/com/ronen/sagy/firevest/MainActivity.java
@@ -1,14 +1,72 @@
 package com.ronen.sagy.firevest;
 
+// android is deprecated, studio suggested to use androidx https://developer.android.com/jetpack/androidx
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.ronen.sagy.firevest.adapters.ViewPagerAdapter;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements OnNavigationItemSelectedListener {
+    private FirebaseAuth mAuth;
+
+    private Context mContext;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        mContext = this;
+
+        BottomNavigationView bnv = findViewById(R.id.bottom_navigation);
+
+        ArrayList<Fragment> fragList = new ArrayList<>();
+//        fragList.add(new AccountFragment());
+//        fragList.add(new SwipeViewFragment());
+//        fragList.add(new ActivityFragment());
+        ViewPagerAdapter pagerAdapter = new ViewPagerAdapter(fragList, getSupportFragmentManager());
+        viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setOffscreenPageLimit(3);
+        bnv.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//        switch (menuItem.getItemId()) {
+//            case R.id.account:
+//                viewPager.setCurrentItem(0);
+//                break;
+//            case R.id.fire:
+//                viewPager.setCurrentItem(1);
+//                break;
+//            case R.id.chat:
+//                viewPager.setCurrentItem(2);
+//                break;
+//        }
+        return true;
     }
 }
