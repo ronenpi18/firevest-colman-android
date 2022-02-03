d49445304479fd8791b8c5737876099f141ccd2d
Ronen Pinhasov <ronen.pinhasov@cyberark.com>
2022-02-03 16:24:23 +0200

    firebase login added

--- app/src/main/java/com/ronen/sagy/firevest/adapters/ViewPagerAdapter.java
+++ app/src/main/java/com/ronen/sagy/firevest/adapters/ViewPagerAdapter.java
@@ -1,25 +1,31 @@
 package com.ronen.sagy.firevest.adapters;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
-/**
- * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
- * sequence.
- */
 public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
     private List<Fragment> fragmentList;
+    private final List<String> listTitles = new ArrayList<>();
 
     public ViewPagerAdapter(List<Fragment> fragmentList, FragmentManager fm) {
-        super(fm);
-        this.fragmentList = fragmentList;
+            super(fm);
+            this.fragmentList = fragmentList;
+    }
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
     }
 
+    public void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+    }
     @Override
     public Fragment getItem(int position) {
         if(position >= 0 && position < fragmentList.size()) {
@@ -27,6 +33,12 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
         }
 
         return new Fragment();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return listTitles.get(position);
     }
 
     @Override
