b991589e506112b8ccd594a48189c68a8bb51f6b
Ronen Pinhasov <ronenpi18@gmail.com>
2022-03-18 21:39:36 +0200

    tinder a like card fixes

--- app/src/main/java/com/ronen/sagy/firevest/entities/TinderCard.java
+++ app/src/main/java/com/ronen/sagy/firevest/entities/TinderCard.java
@@ -6,80 +6,49 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.mindorks.placeholderview.SwipePlaceHolderView;
-import com.mindorks.placeholderview.annotations.Layout;
-import com.mindorks.placeholderview.annotations.Resolve;
-import com.mindorks.placeholderview.annotations.View;
-import com.mindorks.placeholderview.annotations.swipe.SwipeCancelState;
-import com.mindorks.placeholderview.annotations.swipe.SwipeIn;
-import com.mindorks.placeholderview.annotations.swipe.SwipeInState;
-import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
-import com.mindorks.placeholderview.annotations.swipe.SwipeOutState;
+
 import com.ronen.sagy.firevest.R;
 import com.ronen.sagy.firevest.services.model.AppDatabase;
 import com.ronen.sagy.firevest.services.model.Users;
 
 
-@Layout(R.layout.adapter_tinder_card)
 public class TinderCard {
 
-    @View(R.id.profileImageView)
     private ImageView profileImageView;
 
-    @View(R.id.nameAgeTxt)
     private TextView nameAgeTxt;
 
-    @View(R.id.locationNameTxt)
     private TextView locationNameTxt;
     private AppDatabase database;
 
     private Users mProfile;
     private Context mContext;
-    private SwipePlaceHolderView mSwipeView;
 
-    public TinderCard(Context context, Users profile, SwipePlaceHolderView swipeView) {
+    public TinderCard(Context context, Users profile) {
         mContext = context;
         mProfile = profile;
-        mSwipeView = swipeView;
         database = AppDatabase.getInstance(mContext);
 
     }
 
-    @Resolve
     private void onResolved(){
         Glide.with(mContext).load(mProfile.getImageUrl()).into(profileImageView);
         nameAgeTxt.setText(mProfile.getUsername() + ", " + mProfile.getInvestmentStageOrCapital());
         locationNameTxt.setText(mProfile.getFieldOfWork());
     }
 
-    @SwipeOut
-    private void onSwipedOut(){
-        Log.d("EVENT", "onSwipedOut");
-        mSwipeView.addView(this);
-    }
-
-    @SwipeCancelState
-    private void onSwipeCancelState(){
-        Log.d("EVENT", "onSwipeCancelState");
-    }
-
-    @SwipeIn
-    private void onSwipeIn(){
-        Log.d("EVENT", "onSwipedIn");
-    }
-
-    @SwipeInState
-    private void onSwipeInState(){
-        Log.d("EVENT", "onSwipeInState");
+//    @SwipeInState
+//    private void onSwipeInState(){
+//        Log.d("EVENT", "onSwipeInState");
+////        database.swipedDao().insertAll(mProfile.getEmailId(),
+////                mProfile.getId(), mProfile.getUsername(), "Hey, I would like to invest!");
+//    }
+//
+//    @SwipeOutState
+//    private void onSwipeOutState(){
+//        Log.d("EVENT", "onSwipeOutState");
+//
 //        database.swipedDao().insertAll(mProfile.getEmailId(),
-//                mProfile.getId(), mProfile.getUsername(), "Hey, I would like to invest!");
-    }
-
-    @SwipeOutState
-    private void onSwipeOutState(){
-        Log.d("EVENT", "onSwipeOutState");
-
-        database.swipedDao().insertAll(mProfile.getEmailId(),
-                mProfile.getId(), mProfile.getUsername(), "");
-    }
+//                mProfile.getId(), mProfile.getUsername(), "");
+//    }
 }
